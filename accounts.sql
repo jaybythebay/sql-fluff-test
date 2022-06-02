0ff5f647339bdeb1f828b7d3fb4555b59067fffd
Jay Rosenthal <jay.rosenthal@panther.io>
2022-06-02 04:09:25 -0700

    Added rules, template, ignore, and sql

--- accounts.sql
+++
@@ -1,5 +0,0 @@
-
-SELECT id,name AS my_name, a_really_long_field_name, anotherreallylongfieldname, imgonnaputalongnameheretogettotheend, something_ELSE,
-    , sum(metric)
-    , a +b
-from my_db.my_s.table_name
