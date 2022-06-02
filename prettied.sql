48531a57ec5b591b2c322ee19d5a56f27b496ef8
Jay Rosenthal <jay.rosenthal@panther.io>
2022-06-02 04:09:54 -0700

    rules, sql

---
+++ prettied.sql
@@ -0,0 +1,5 @@
+
+SELECT id,name AS my_name, a_really_long_field_name, anotherreallylongfieldname, imgonnaputalongnameheretogettotheend, something_ELSE,
+    , sum(metric)
+    , a +b
+from my_db.my_s.table_name
