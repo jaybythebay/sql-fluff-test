10671c773151e6a3d69e3043918102d05aece9db
Jay Rosenthal <jay.rosenthal@panther.io>
2022-06-02 14:57:09 -0700

    changed config and path

--- models/prettied.sql
+++ models/prettied.sql
@@ -1,13 +1,6 @@
-SELECT
-    id
-    , name AS my_name
-    , a_really_long_field_name
-    , anotherreallylongfieldname
-    , imgonnaputalongnameheretogettotheend
-    , something_else
-    , "something" AS should_be_single_quotes
-    , SUM(metric)
-    , a + b
-    , COUNT(*)
-FROM my_db.my_s.table_name
-WHERE a IS NULL
+
+SELECT id,name AS my_name, a_really_long_field_name, anotherreallylongfieldname, imgonnaputalongnameheretogettotheend, something_ELSE,
+    sum(metric)
+    , a +b, count(1), "something" AS should_be_single_quotes, Max(some_field) AS dfsdf
+from my_db.my_s.table_name
+WHERE a=NULL
