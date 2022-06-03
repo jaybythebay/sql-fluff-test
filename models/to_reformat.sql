d433d6e3e3e0da677aec46a634e6e8e3dbc4b464
jaybythebay <jaywallacerosenthal@gmail.com>
2022-06-03 15:54:19 -0700

    Print Usable Output (#4)
    Print the SQLFluff default table on the lint step

--- models/to_reformat.sql
+++ models/to_reformat.sql
@@ -6,9 +6,9 @@ SELECT
 	, imgonnaputalongnameheretogettotheend
 	, something_else
 	, "something" AS should_be_single_quotes
-	, SUM(metric) AS thing
-	, a + b AS addition_thing
-	, COUNT(*) AS another_thing
+	, SUM(metric) AS metric_name
+	, a + b AS addition
+	, COUNT(*) AS a_count
 	, MAX(some_field) AS dfsdf
 FROM my_db.my_s.table_name
 WHERE a IS NULL
