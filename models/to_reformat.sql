e6f99840ed05bd4cbf48640548d612f7f9a11f1f
Jay Rosenthal <jay.rosenthal@panther.io>
2022-06-03 15:16:20 -0700

    Add readme, fix everything to see if it pass tests

--- models/to_reformat.sql
+++ models/to_reformat.sql
@@ -5,10 +5,10 @@ SELECT
 	, anotherreallylongfieldname
 	, imgonnaputalongnameheretogettotheend
 	, something_else
-    , "something" AS should_be_single_quotes
-    , SUM(metric)
-	, a + b
-	, COUNT(*)
+	, "something" AS should_be_single_quotes
+	, SUM(metric) AS thing
+	, a + b AS addition_thing
+	, COUNT(*) AS another_thing
 	, MAX(some_field) AS dfsdf
 FROM my_db.my_s.table_name
 WHERE a IS NULL
