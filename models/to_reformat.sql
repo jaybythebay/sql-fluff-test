9ddef84f2a27747f374065d2375aac07f61a5e8f
Jay Rosenthal <jay.rosenthal@panther.io>
2022-06-03 15:34:35 -0700

    make it fail

--- models/to_reformat.sql
+++ models/to_reformat.sql
@@ -1,14 +1,6 @@
-SELECT
-	id
-	, name AS my_name
-	, a_really_long_field_name
-	, anotherreallylongfieldname
-	, imgonnaputalongnameheretogettotheend
-	, something_else
-	, "something" AS should_be_single_quotes
-	, SUM(metric) AS thing
-	, a + b AS addition_thing
-	, COUNT(*) AS another_thing
-	, MAX(some_field) AS dfsdf
-FROM my_db.my_s.table_name
-WHERE a IS NULL
+
+SELECT id,name AS my_name, a_really_long_field_name, anotherreallylongfieldname, imgonnaputalongnameheretogettotheend, something_ELSE,
+    sum(metric)
+    , a +b, count(1), "something" AS should_be_single_quotes, Max(some_field) AS dfsdf
+from my_db.my_s.table_name
+WHERE a=NULL
